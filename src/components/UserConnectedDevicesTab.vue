844d4a4dd41e93b0a85e759992787838c2afc99b
Matthew Brimmer <mbrimmer@it-mbp-mbrimmer.local>
2018-11-11 09:15:24 -0800

    Connnected D

--- src/components/UserConnectedDevicesTab.vue
+++ src/components/UserConnectedDevicesTab.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    Concected Devives
+    <div class="image-group">
+      <img class="connected-image" src="../assets/Nike-Running-App.png" />
+      <img class="connected-image" src="../assets/ihealth-logo.png" />
+    </div>
+    <div class="image-group">
+      <img class="connected-image" src="../assets/garmin-logo.jpeg" />
+      <img  class="connected-image" src="../assets/headspace-logo.png" />
+    </div>
   </div>
 </template>
+
+
+<style>
+  .connected-image {
+    width: 175px;
+    height: 175px;
+    border-radius: 20px
+  }
+
+  .image-group {
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 25px;
+  }
+</style>
