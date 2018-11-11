e2c9396b7c34881e63eea5ea7feeba3091deb258
DonaldPeat <donaldpeat@gmail.com>
2018-11-10 17:52:31 -0800

    fix: set routes & update paths

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import './plugins/vuetify'
 import App from './App.vue'
-import Bar from './components/Bar.vue'
+import User from './components/User.vue'
+import Enterprise from './components/Enterprise.vue'
+import Provider from './components/Provider.vue'
+
+
 const routes = [
-  { path: '/bar', component: Bar }
+  { path: '/user', component: User },
+  { path: '/enterprise', component: Enterprise },
+  { path: '/provider', component: Provider }
 ]
 
 const router = new VueRouter({
-  routes // short for `routes: routes`
+  routes
 })
 
 Vue.config.productionTip = false
