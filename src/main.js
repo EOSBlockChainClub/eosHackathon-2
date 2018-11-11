23071ae9fe224d969f180501b9bc10d665f7566c
DonaldPeat <donaldpeat@gmail.com>
2018-11-10 17:46:08 -0800

    feature: set up basic router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import './plugins/vuetify'
 import App from './App.vue'
+import Bar from './components/Bar.vue'
+const routes = [
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
